d34a63049f0b54148d208f906866454a0d5212c1
DanStudio! <78805018+dan-studio@users.noreply.github.com>
2022-09-03 15:13:22 +0900

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import main from './modules/mainSlice'
 import myPage from "./modules/myPageSlice";
-import main from './modules/mainSlice'
+
 const store = configureStore({
   reducer: {
     main,
